6e1df70d7f9d3debe55abfa8dcb8c88d688b1eae
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2020-10-31 22:07:56 +0200

    Added PWA Functionality

--- next.config.js
+++ next.config.js
@@ -1,47 +1,25 @@
-const withOffline = require("next-offline");
+const withPWA = require("next-pwa");
 
-module.exports = withOffline({
+module.exports = withPWA({
+  pwa: {
+    dest: "public"
+  },
   publicRuntimeConfig: {
     defaultCardsCount: 5,
     restTimeAfterWin: 3,
     cardsByRowCount: 5,
     allCardReveal: 5
-  },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(jpg|png|svg)$/,
-      use: {
-        loader: "url-loader",
-        options: {
-          limit: 25000
-        }
-      }
-    });
-    return config;
-  },
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? "service-worker.js"
-      : "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200
-          }
-        }
-      }
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/service-worker.js",
-        destination: "/_next/static/service-worker.js"
-      }
-    ];
   }
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.(jpg|png|svg)$/,
+  //     use: {
+  //       loader: "url-loader",
+  //       options: {
+  //         limit: 25000
+  //       }
+  //     }
+  //   });
+  //   return config;
+  // }
 });
