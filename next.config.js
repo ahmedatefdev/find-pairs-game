0f7a389e7725f8c94552a2799dc00c5ecdbe7c2e
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2020-11-01 13:06:05 +0200

    update manifest & config

--- next.config.js
+++ next.config.js
@@ -9,5 +9,18 @@ module.exports = withPWA({
     restTimeAfterWin: 3,
     cardsByRowCount: 5,
     allCardReveal: 5
+  },
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]"
+        }
+      }
+    });
+    return config;
   }
 });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,10 @@
-import App, { AppProps } from 'next/app';
-import { Provider, useSelector } from "react-redux";
+import { AppProps } from 'next/app';
+import { useSelector } from "react-redux";
 import { ThemeProvider } from 'styled-components'
-import * as React from 'react';
+import React from 'react';
 import { darkTheme, lightTheme } from '../styles/vars';
 import { wrapper } from '../redux/Store';
 import IState from '../redux/types/IState';
-
-// export interface IAppProps extends AppProps {
-//   store: any
-// }
-// export interface IAppState {
-// }
-// // class MyApp extends App<IAppProps, {}, IAppState> {
-// //   render() {
-// //     const { Component, pageProps, store } = this.props;
-// //     return (
-// //       <ThemeProvider theme={this.state.theme === 'light' ? lightTheme : darkTheme}>
-// //         <Component {...pageProps} />
-// //       </ThemeProvider >
-
-// //     );
-// //   }
-// // }
 
 
 const MyApp = (props: AppProps) => {
@@ -37,7 +20,3 @@ const MyApp = (props: AppProps) => {
 
 
 export default wrapper.withRedux(MyApp);
-// export default MyApp
-
-
-
